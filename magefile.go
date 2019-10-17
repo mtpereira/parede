462397eafbb14c5968e5c377c2f08f5b514e7360
Manuel Tiago Pereira <mt.pereira@gmail.com>
2019-10-17 19:37:36 +0100

    Install hugo-post-preview
    Adds a dependency to the Preview target that installs the required
    tooling.
    A logging function was also added so that more verbose output is shown
    when `mage` is ran with `-verbose`.

--- magefile.go
+++ magefile.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"io/ioutil"
 
+	"github.com/magefile/mage/mg"
 	"github.com/magefile/mage/sh"
 	"github.com/magefile/mage/target"
 )
@@ -30,6 +31,8 @@ func Publish() error {
 }
 
 func Content() error {
+	verbose("generating content...")
+
 	err := sh.Run("hugo")
 	if err != nil {
 		return err
@@ -39,6 +42,8 @@ func Content() error {
 }
 
 func Preview() error {
+	mg.Deps(installHugoPostPreview)
+
 	posts, err := changedPosts()
 	if err != nil {
 		return err
@@ -54,7 +59,16 @@ func Preview() error {
 	return nil
 }
 
+func installHugoPostPreview() {
+	verbose("installing hugo-post-preview...")
+
+	sh.Run("go", "get", "github.com/mtpereira/hugo-post-preview")
+	sh.Run("go", "install", "github.com/mtpereira/hugo-post-preview/cmd/hugo-post-preview")
+}
+
 func hugoPostPreview(post string) error {
+	verbose(fmt.Sprintf("creating preview for post %s", post))
+
 	err := sh.Run("hugo-post-preview", "-filename", previewPath(post), "-post", post, "-timeout", "3s")
 	if err != nil {
 		return err
@@ -72,13 +86,17 @@ func changedPosts() ([]string, error) {
 	}
 
 	for _, f := range files {
+		if !f.IsDir() {
+			continue
+		}
+
 		file := f.Name()
 		changed, err := target.Path(previewPath(file), postPath(file))
 		if err != nil {
 			return nil, err
 		}
 
-		if f.IsDir() && changed {
+		if changed {
 			posts = append(posts, file)
 		}
 	}
@@ -89,3 +107,9 @@ func changedPosts() ([]string, error) {
 func previewPath(post string) string { return fmt.Sprintf("%s/p_%s.png", previewsDir, post) }
 
 func postPath(post string) string { return fmt.Sprintf("%s/%s/index.html", postsDir, post) }
+
+func verbose(msg string) {
+	if mg.Verbose() {
+		fmt.Println(msg)
+	}
+}
